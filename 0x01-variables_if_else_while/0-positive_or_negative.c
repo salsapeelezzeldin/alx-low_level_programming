855a0b35331825c4dfd2986023b1715b59577a52
salsapeelezzeldin <salsapeelezzeldin@gmail.com>
2022-12-10 02:15:19 +0200

    variables_if_else_while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
@@ -17,10 +17,10 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
-	{					
+	{
 		printf("%d is positive\n", n);
 	}
-	else						
+	else
 		printf("%d is zero\n", n);
 	return (0);
 }
